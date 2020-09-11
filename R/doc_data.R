ece714503ae4d865e715743d0821be6dfe5452e4
Jay Burns <jburns@sruc.ac.uk>
2020-09-11 16:52:46 +0100

    initial

---
+++ R/doc_data.R
@@ -0,0 +1,17 @@
+#' Example data: partial rankings
+#'
+#' A dataset containing 5 partial rankings
+#'              of 6 items. This is the example used by
+#'              Cook et al (2007).
+#'
+#' @format A data frame of 20 rows and 3 columns
+#' \describe{
+#'  \item{Item}{Character values giving one of 6 items}
+#'  \item{Reviewer}{Character values giving one of 5 reviewers}
+#'  \item{Ranking}{Numeric values giving a rank position}
+#' }
+#' @source Cook, W.D., Golany, B., Penn, M. and Raviv, T., 2007.
+#'         Creating a consensus ranking of proposals from reviewers’
+#'         partial ordinal rankings. Computers & Operations Research,
+#'         34, pp.954-965.
+"cook_example"
